7f7cd886f22e71786bc35cdec914bd75741ea49a
gosse123 <gossenahounou@gmail.com>
2026-05-09 21:49:08 +0000

    desactivation de la parti pupup

--- src/platform/UI/app.py
+++ src/platform/UI/app.py
@@ -3,7 +3,7 @@ from controller.controller import Controller
 from screens.connection import ConnectionScreen
 from screens.transfer import TransferScreen
 from screens.popup import ConnectionRequestPopup, FileTransferRequestPopup
-from tkinter import messagebox as tk_messagebox  
+from tkinter import messagebox as tk_messagebox
 
 class App(ctk.CTk):
     def __init__(self):
@@ -31,27 +31,27 @@ class App(ctk.CTk):
         self.transfer_screen.pack(fill="both", expand=True)
 
         # Test popups (tu peux les appeler depuis des boutons)
-        self.after(2000, self.test_popups)
+        #self.after(2000, self.test_popups)
 
-    def test_popups(self):
-        # Exemple de popup connexion
-        ConnectionRequestPopup(
-            self,
-            username="Madeleine_Y",
-            ip="192.168.1.5",
-            on_accept=lambda: self.controller.accept_connection("Madeleine_Y", "192.168.1.5"),
-            on_decline=lambda: self.controller.decline_connection("Madeleine_Y")
-        )
+    # def test_popups(self):
+    #     # Exemple de popup connexion
+    #     ConnectionRequestPopup(
+    #         self,
+    #         username="Madeleine_Y",
+    #         ip="192.168.1.5",
+    #         on_accept=lambda: self.controller.accept_connection("Madeleine_Y", "192.168.1.5"),
+    #         on_decline=lambda: self.controller.decline_connection("Madeleine_Y")
+    #     )
 
-        # Exemple de popup transfert
-        FileTransferRequestPopup(
-            self,
-            sender="Gerard_BIT",
-            filename="ubuntu.iso",
-            size="(2.3 Go)",
-            on_accept=lambda: self.controller.accept_file_transfer("Gerard_BIT", "ubuntu.iso", "2.3 Go"),
-            on_decline=lambda: print("Transfert refusé")
-        )
+    #     # Exemple de popup transfert
+    #     FileTransferRequestPopup(
+    #         self,
+    #         sender="Gerard_BIT",
+    #         filename="ubuntu.iso",
+    #         size="(2.3 Go)",
+    #         on_accept=lambda: self.controller.accept_file_transfer("Gerard_BIT", "ubuntu.iso", "2.3 Go"),
+    #         on_decline=lambda: print("Transfert refusé")
+    #     )
 
 
 if __name__ == "__main__":
