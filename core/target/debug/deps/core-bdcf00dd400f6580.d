dd5727f1bdbb4d4369155301e40e0246a150c4ce
YaogoGerard <gerardwyaogo@gmail.com>
2026-06-17 08:48:00 +0000

    init: remove target from tracking

--- core/target/debug/deps/core-bdcf00dd400f6580.d
+++
@@ -1,5 +0,0 @@
-/home/gerard/Documents/Burkina-Open-Place/Toole/core/target/debug/deps/core-bdcf00dd400f6580.d: src/lib.rs
-
-/home/gerard/Documents/Burkina-Open-Place/Toole/core/target/debug/deps/libcore-bdcf00dd400f6580.rmeta: src/lib.rs
-
-src/lib.rs:
