5c19c56dbdbe998420db02af5b4a648887d4bc33
YaogoGerard <gerardwyaogo@gmail.com>
2026-06-26 20:48:07 +0000

    Discovery:refont

--- core/src/lib.rs
+++
@@ -1,9 +0,0 @@
-// Point d'entrée de la bibliothèque core.
-//
-// Réexporte tous les modules publics : error, utils, discovery, transfer, network.
-//
-// Définit aussi :
-//   - Trait UI          : pont entre core et le frontend (log, set_progress, on_peer_found, on_transfer_done, on_error)
-//   - struct Peer       : nom + adresse d'un appareil détecté
-//   - enum Mode         : Send / Receive
-//   - struct TransferStatus : filename, total_bytes, received_bytes, speed
--- core/src/main.rs
+++
@@ -1,6 +0,0 @@
-mod utils;
-
-fn main() {
-    println!("SSID : {}", utils::gen_ssid());
-    println!("Nom : {}", utils::current_hostname());
-}
