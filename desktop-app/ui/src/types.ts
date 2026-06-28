f3b1b85b9eca563e94614610ccadbe581e6623e6
YaogoGerard <gerardwyaogo@gmail.com>
2026-06-28 15:21:34 +0000

    feat: ajouter interfaces partagées (Peer, FileEntry)

---
+++ desktop-app/ui/src/types.ts
@@ -0,0 +1,14 @@
+export interface Peer {
+  hostname: string;
+  addr: string;
+}
+
+export interface FileEntry {
+  path: string;
+  name: string;
+  size?: number;
+}
+
+export interface PeerListPayload {
+  peers: Peer[];
+}
