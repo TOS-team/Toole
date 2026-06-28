20e2d5792f93de3da25e94805dc7995069fad480
YaogoGerard <gerardwyaogo@gmail.com>
2026-06-28 05:05:38 +0000

    Discovery: UI trait on Peer  trait

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -2,10 +2,16 @@ pub mod error;
 pub use error::ToolError;
 pub mod utils;
 pub mod discovery;
+use serde::Serialize;
 
-
-#[derive(Debug,Clone)]
+#[derive(Debug,Clone,Serialize)]
 pub struct Peer{
     pub hostname:String,
     pub addr:String,
+}
+
+pub trait UI: Send + Sync {
+    fn log(&self, msg: &str);
+    fn peer_found(&self,peer:&Peer);
+    fn peer_lost(&self,hostname:&str);
 }
