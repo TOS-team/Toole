6e13072cbf6768bdc0533fab3f01685c164bbb1c
YaogoGerard <gerardwyaogo@gmail.com>
2026-06-28 15:26:36 +0000

    feat: ajouter déclaration de module pour les fichiers .vue

--- desktop-app/ui/src/vite-env.d.ts
+++ desktop-app/ui/src/vite-env.d.ts
@@ -6,4 +6,10 @@ declare module "*.vue" {
   export default component;
 }
 
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<object, object, unknown>;
+  export default component;
+}
+
 declare module "*.css";
