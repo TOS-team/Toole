685a526756cd17ee5cdd89a23f88ddebc1deab93
YaogoGerard <gerardwyaogo@gmail.com>
2026-08-10 23:25:42 +0000

    front: nettoyer les déclarations vite-env

--- desktop-app/ui/src/vite-env.d.ts
+++ desktop-app/ui/src/vite-env.d.ts
@@ -6,10 +6,4 @@ declare module "*.vue" {
   export default component;
 }
 
-declare module "*.vue" {
-  import type { DefineComponent } from "vue";
-  const component: DefineComponent<object, object, unknown>;
-  export default component;
-}
-
 declare module "*.css";
